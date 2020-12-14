855f5ac88a8fe2fb924dbdecc31d744959c1db70
Deep <deep>
2020-12-14 19:24:32 +0530

    added ui test case

---
+++ app/src/androidTest/java/com/flickrdemo/HomeScreenTest.kt
@@ -0,0 +1,28 @@
+package com.flickrdemo
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import com.flickrdemo.view.home.MainActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class HomeScreenTest {
+
+    @get:Rule
+    public val mActivityRule: ActivityTestRule<MainActivity> =
+        ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun existRecyclerView() {
+        onView(withId(R.id.rvImages)).perform(click())
+    }
+}
